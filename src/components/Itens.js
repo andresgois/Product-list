ccafc43370f25418514df3ca7b7897adfc4e12aa
Andre gois <andre.s.gois3@gmail.com>
2021-11-19 20:36:40 -0300

    putting items on a card

--- src/components/Itens.js
+++ src/components/Itens.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image,StyleSheet,Text, View } from 'react-native';
 
 class Itens extends Component {
 
@@ -10,11 +10,68 @@ constructor(props){
 
  render(){
    return(
-    <Text>
-      Detalhe Item
-    </Text>
+    <View style={[style.card, style.shadowProp]}>
+      <Image style={style.img} source={{ uri: this.props.item.image_link}} />
+      <View style={style.info}>
+        <Text style={style.name}>{this.props.item.name}</Text>
+        <Text style={style.brand}>{this.props.item.brand}</Text>
+        <Text style={style.price}>R$ {this.props.item.price}</Text>
+      </View>
+    </View>
    );
   }
  }
 
 export default Itens;
+
+const style=StyleSheet.create({
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    paddingVertical: 5,
+    paddingHorizontal: 25,
+    width: '90%',
+    marginVertical: 10,
+  },
+  shadowProp: {
+    shadowColor: '#ccc',
+    shadowOffset: {width: -2, height: 10},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  img: {
+    height: 100, 
+    width: 100,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    shadowOffset: { height: 1, width: 10},
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  info: {
+    flex: 1,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textDecorationStyle: 'solid',
+    textDecorationLine:'underline'
+  },
+  brand: {
+    fontWeight: '500',
+  },
+  price: {
+    fontWeight: 'bold',
+  }
+})
--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import api from "../API/api";
 import Itens from "./Itens";
 
@@ -12,7 +11,7 @@ export default class ListaItens extends Component {
   }
 
   componentDidMount(){
-    api.get('?brand=covergirl&product_type=lipstick')
+    api.get('?brand=maybelline')
         .then( res => {
           let dados = res.data
           this.setState({listaItens: dados})
@@ -23,13 +22,19 @@ export default class ListaItens extends Component {
   render(){
     
     return(
-      <View>
+      <ScrollView style={styles.container}>
         {
           this.state.listaItens.map( lista =>  (
-            <Text key={lista.id}>{lista.name}</Text>
+            <Itens key={lista.name} item={lista} />
           ))
         }
-      </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ab78da'
+  }
+})
